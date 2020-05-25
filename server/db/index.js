28f46b52c69b196396c839c829a6535a2a44f3f7
Brady Peterson <bpeterson1663@gmail.com>
2020-05-24 22:50:00 -0500

    feat(server): initial configuration for server

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+const connectionString = process.env.DB_URI
+
+mongoose
+  .connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((e) => console.error('Connection error: ', e.message))
+
+const db = mongoose.connection
+
+module.exports = db
