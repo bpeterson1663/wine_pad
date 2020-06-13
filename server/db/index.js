e8a436ff49959005cb64d192fcf41f0db8bed577
Brady Peterson <bpeterson1663@gmail.com>
2020-06-13 07:44:08 -0500

    fix(deprecated method): fix deprecated warning from mongoose

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ dotenv.config()
 const connectionString = process.env.DB_URI
 
 mongoose
-  .connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(connectionString, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
   .catch((e) => console.error('Connection error: ', e.message))
 
 const db = mongoose.connection
